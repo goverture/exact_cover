8e996fe03a800872b6385db35e4c9c48e797fddb
Jerome <marhic.jerome@gmail.com>
2025-02-19 16:02:06 +0000

    solve sudoku with v2

--- examples/sudoku/sudoku_solver.go
+++ examples/sudoku/sudoku_solver.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
 
 	goverture "github.com/goverture/exact_cover"
 )
@@ -40,15 +38,15 @@ func printGrid(grid [Size][Size]int) {
 func main() {
 	fmt.Println("Initial Sudoku Grid:")
 	var testGrid = [Size][Size]int{
-		{0, 0, 0, 6, 7, 8, 9, 1, 2},
-		{6, 7, 2, 1, 9, 5, 3, 4, 8},
-		{1, 9, 8, 3, 4, 2, 5, 6, 7},
-		{8, 5, 9, 7, 6, 1, 4, 2, 3},
-		{4, 2, 6, 8, 5, 3, 7, 9, 1},
-		{7, 1, 3, 9, 2, 0, 8, 5, 6},
-		{9, 6, 1, 5, 3, 7, 2, 8, 4},
-		{2, 8, 7, 4, 1, 9, 6, 3, 5},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{5, 3, 0, 0, 7, 0, 0, 0, 0},
+		{6, 0, 0, 1, 9, 5, 0, 0, 0},
+		{0, 9, 8, 0, 0, 0, 0, 6, 0},
+		{8, 0, 0, 0, 6, 0, 0, 0, 3},
+		{4, 0, 0, 8, 0, 3, 0, 0, 1},
+		{7, 0, 0, 0, 2, 0, 0, 0, 6},
+		{0, 6, 0, 0, 0, 0, 2, 8, 0},
+		{0, 0, 0, 4, 1, 9, 0, 0, 5},
+		{0, 0, 0, 0, 8, 0, 0, 7, 9},
 	}
 
 	printGrid(testGrid)
@@ -98,49 +96,17 @@ func main() {
 		}
 	}
 
-	sparseMatrix := goverture.SparseMatrixFromArray(choices)
+	columns, nodes := goverture.BuildDLX(choices)
+	visitor := func(solution []int) {
+		optionIndex := make([]int, len(solution))
+		for i, x := range(solution) {
+			for nodes[x].Top > 0 {
+				x = x - 1
+			}
+			optionIndex[i] = -nodes[x].Top
+		}
+		fmt.Println("Solution found : ")
 
-	solutionsChan := goverture.SolveDLX(context.Background(), sparseMatrix, -1*time.Second)
-
-	// Collect all solutions into a slice
-	var solutions []goverture.Solution
-	for sol := range solutionsChan {
-		solutions = append(solutions, sol)
-	}
-
-	// Define the number of expected solutions
-	expectedSolutionsCount := 1 // Typically, a Sudoku puzzle has a unique solution
-
-	if len(solutions) != expectedSolutionsCount {
-		fmt.Printf("Expected %d solution(s), got %d\n", expectedSolutionsCount, len(solutions))
-	} else {
-		fmt.Printf("Found %d solution(s)\n", len(solutions))
-	}
-
-	// slicesEqual := func(a, b []int) bool {
-	// 	if len(a) != len(b) {
-	// 		return false
-	// 	}
-	// 	for i := range a {
-	// 		if a[i] != b[i] {
-	// 			return false
-	// 		}
-	// 	}
-	// 	return true
-	// }
-
-	// findRowIndex finds the index of a given row in the matrix.
-	// findRowIndex := func(matrix [][]int, row []int) (int, bool) {
-	// 	for i, r := range matrix {
-	// 		if slicesEqual(r, row) {
-	// 			return i, true
-	// 		}
-	// 	}
-	// 	return -1, false
-	// }
-
-	// Process each solution to reconstruct and display the completed Sudoku grid
-	for idx, sol := range solutions {
 		// Initialize an empty Sudoku grid
 		var solvedGrid [Size][Size]int
 
@@ -152,21 +118,16 @@ func main() {
 		}
 
 		// Iterate over each row in the solution
-		for _, row := range sol.Matrix {
-			// Find the index of this row in the exact cover matrix
-			choiceIndex, found := goverture.FindRowIndex(sparseMatrix, row)
-			if !found {
-				fmt.Printf("Row %v not found in the matrix\n", row)
-				continue
-			}
-
+		for _, i := range optionIndex {
 			// Map the choice index to the corresponding cell and number
-			choice := choiceToCell[choiceIndex]
+			choice := choiceToCell[i]
 			solvedGrid[choice.Row][choice.Col] = choice.Num
 		}
 
 		// Display the solved grid
-		fmt.Printf("\nSolution %d:\n", idx+1)
 		printGrid(solvedGrid)
+		fmt.Println("--------")
 	}
+
+	goverture.SolveExactCover(columns, nodes, []int{}, visitor)
 }
